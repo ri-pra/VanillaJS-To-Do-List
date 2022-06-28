6819cf2235b1e3395cf621a85dda117eec937129
ri-pra <riwajprasai03@gmail.com>
2022-06-28 19:02:34 +0545

    basic small screen nav

---
+++ index.js
@@ -0,0 +1,16 @@
+let nav_open = true;
+function toggleNav() {
+  const nav = document.getElementsByClassName("nav")[0];
+  const nav_toggler = document.getElementsByClassName("nav__toggler")[0];
+  if (nav_open) {
+    nav.style.width = "0px";
+    nav.style.padding = "0";
+    nav_toggler.style.left = "20px";
+  } else {
+    nav.style.width = "500px";
+    nav.style.padding = "20px";
+    nav_toggler.style.left = "450px";
+  }
+  nav_open = !nav_open;
+}
+//https://img.icons8.com/ios-glyphs/344/circled-chevron-up.png
